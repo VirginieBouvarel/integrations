6dbf789d0c67fcc0f0efdc629d4a53c9557ec797
Virginie <vbouvarel@lilo.org>
2021-01-07 16:56:03 +0100

    fadeInUp keys WIP

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+const keys = document.querySelector('.key-points');
+
+window.addEventListener('scroll', () => {
+    animateOnScroll(keys);
+});
+
+function animateOnScroll(target) {
+    if(window.scrollY >= target.offsetTop - 600) {
+        target.classList.add('animated');
+    } 
+    
+    
+}
+
+
+   
