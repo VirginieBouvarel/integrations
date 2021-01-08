f88f1269bd1ec57274eca0b020caa351e369454e
Virginie <vbouvarel@lilo.org>
2021-01-08 12:36:51 +0100

    💄 animation keys ok + partners ok

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 "use strict";
 
 const keys = document.querySelector('.key-points');
+const partners = document.querySelector('.partners');
 
 window.addEventListener('scroll', () => {
     animateOnScroll(keys);
+    animateOnScroll(partners);
 });
 
 function animateOnScroll(target) {
-    if(window.scrollY >= target.offsetTop - 600) {
+    if(window.scrollY >= target.offsetTop - 1000) {
         target.classList.add('animated');
     } 
     
